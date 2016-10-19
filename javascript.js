9b4f9ade310670532c4ac438f269b192d69239ac
Start Coding Student 1 <nicolas.clocksin@west-mec.org>
2016-10-19 15:24:51 -0700

    Added materialize to the files. Also edited the carousel so you have to manually slide. Made the website a single page. Also added font styles and began using JSON to create a person carousel.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,26 @@
-$('.carousel').carousel({full_width: true});
-        
+$('.carousel.carousel-slider').carousel({full_width: true});
+$('.carousel.carousel-slider').carousel('next');
+$('.carousel.carousel-slider').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel.carousel-slider').carousel('prev');
+$('.carousel.carousel-slider').carousel('prev', 4); // Move prev n times.
+
+
+$(function() {
+
+    $.getJSON('data.json', function(data) {
+        var template = $('#aboutus').html();
+        var html = Mustache.to_html(template, data);
+        $('#aboutcarousel').html(html);
+
+        $('#aboutcarousel').cycle({
+            fx: 'fade',
+            pause: 1,
+            next: '#next_btn',
+            prev: '#prev_btn',
+            speed: 500,
+            timeout: 10000
+        });
+    }); //getJSON
+
+}); //function
